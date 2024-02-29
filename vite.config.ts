26889bb4e9048abc77695f7b587a660c7bde5382
Gagan Chatu <67225429+Gagan-C@users.noreply.github.com>
2024-02-29 04:45:24 -0600

    Update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'https://gagan-c.github.io/GitHubPages_test.github.io/',
   plugins: [react()],
-  build:{
-    outDir:'./build'
-  }
 })
