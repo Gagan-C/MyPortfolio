4276981583229c99305b2520074d47a695c88211
Gagan Chatu <67225429+Gagan-C@users.noreply.github.com>
2024-04-01 21:18:04 -0500

    PRWA-2 added project page with cards and json config

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,37 +1,59 @@
-
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
 import ProjectPlaceholderImage from "../assets/project-placeholder.svg";
-import { Link } from '@mui/material';
+import { Link } from "@mui/material";
 
-function ProjectCard(){
-    return (
-        <Card raised={true} sx={{ maxWidth: 345 }}>
-          <CardMedia
-            component="img"
-            alt="Project img"
-            height="140"
-            image={ProjectPlaceholderImage}
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-            DLT Simulation
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              a c++ based simulation.
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Link href="https://github.com/Gagan-C/DLT-Simulation" target="_blank">github</Link>
-            <Link href="https://github.com/Gagan-C/DLT-Simulation" target="_blank">demo</Link>
-          </CardActions>
-        </Card>
-      );
+function ProjectCard({
+  title,
+  description,
+  link,
+  demo,
+  tools,
+}: {
+  title: string;
+  description: string;
+  link: string;
+  demo: string;
+  tools: string;
+}) {
+  const demoLink =
+    demo === "" ? (
+      <></>
+    ) : (
+      <Link href={demo} target="_blank">
+        demo
+      </Link>
+    );
+
+  return (
+    <Card raised={true} sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        alt="Project img"
+        height="130"
+        image={ProjectPlaceholderImage}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {description}
+        </Typography>
+        <Typography variant="overline" color="text.secondary">
+          {tools}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Link href={link} target="_blank">
+          Source Code
+        </Link>
+        {demoLink}
+      </CardActions>
+    </Card>
+  );
 }
-export default ProjectCard
-
-
+export default ProjectCard;
