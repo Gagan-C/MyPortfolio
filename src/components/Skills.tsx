45957afd51e301b97099e9d3a112e7e52621ad9e
Gagan Chatu <67225429+Gagan-C@users.noreply.github.com>
2024-05-02 04:38:49 -0500

    PRWA-3 added application frameworks to skills page

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -3,39 +3,48 @@ import skillsData from "./skill-list.json";
 import proImg from "../assets/programming.jpeg";
 import dbImg from "../assets/db.jpeg";
 import toolImg from "../assets/tools.jpeg";
+import frameworkImg from "../assets/framework.jpeg"
 
 
 
 function Skills() {
-  return <Grid container spacing={4} p={2}>
-  <Grid item xs >
-    <Box alignContent={"center"}>
+  return <Grid container spacing={4} p={2} rowSpacing={2} columnSpacing={2}>
+  <Grid item xs={12} sm={6} md={4} >
       <Card raised >
           <CardMedia component="img" sx={{ height: 200 }} image={proImg}></CardMedia>
-          <Stack direction={"row"} spacing={1} p={2}>
+          <Stack direction={"row"} spacing={1} p={2} useFlexGap flexWrap="wrap" >
               {skillsData.languages.map((language) => (
                   <Chip label={language.name} color="primary" size="small" />
               ))}
           </Stack>
       </Card>
-      </Box>
   </Grid>
-  <Grid item xs>
+  <Grid item xs={12} sm={6} md={4}>
       <Card raised >
           <CardMedia component="img" sx={{ height: 200 }} image={dbImg}></CardMedia>
-          <Stack direction={"row"} spacing={1} p={2}>
+          <Stack direction={"row"} spacing={1} p={2} useFlexGap flexWrap="wrap" >
               {skillsData.databases.map((db) => (
                   <Chip label={db.name} color="primary" size="small" />
               ))}
           </Stack>
       </Card>
   </Grid>
-  <Grid item xs>
+  <Grid item xs={12} sm={6} md={4}>
       <Card raised>
           <CardMedia component="img" sx={{ height: 200 }} image={toolImg}></CardMedia>
-          <Stack direction={"row"} spacing={1} p={2}>
+          <Stack direction={"row"} spacing={1} p={2} useFlexGap flexWrap="wrap"  >
               {skillsData.tools.map((tool) => (
-                  <Chip label={tool.name} color="primary" size="small" />
+                  <Chip  label={tool.name} color="primary" size="small"  />
+              ))}
+          </Stack>
+      </Card>
+  </Grid>
+  <Grid item xs={12} sm={6} md={4} >
+      <Card raised >
+          <CardMedia component="img" sx={{ height: 200 }} image={frameworkImg}></CardMedia>
+          <Stack direction={"row"} spacing={1} p={2} useFlexGap flexWrap="wrap" >
+              {skillsData.frameworks.map((framework) => (
+                  <Chip label={framework.name} color="primary" size="small" />
               ))}
           </Stack>
       </Card>
