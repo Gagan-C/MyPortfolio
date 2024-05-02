df392460830ed0bf3b1953565fd0713fa94f0b43
Gagan Chatu <67225429+Gagan-C@users.noreply.github.com>
2024-05-02 05:50:33 +0000

    git commit -m "PRWA-3 Initial skills design"

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,5 +1,46 @@
+import {  Box, Card, CardMedia, Chip, Grid, Stack } from "@mui/material";
+import skillsData from "./skill-list.json";
+import proImg from "../assets/programming.jpeg";
+import dbImg from "../assets/db.jpeg";
+import toolImg from "../assets/tools.jpeg";
+
+
+
 function Skills() {
-  return <h1>Skills</h1>;
+  return <Grid container spacing={4} p={2}>
+  <Grid item xs >
+    <Box alignContent={"center"}>
+      <Card raised >
+          <CardMedia component="img" sx={{ height: 200 }} image={proImg}></CardMedia>
+          <Stack direction={"row"} spacing={1} p={2}>
+              {skillsData.languages.map((language) => (
+                  <Chip label={language.name} color="primary" size="small" />
+              ))}
+          </Stack>
+      </Card>
+      </Box>
+  </Grid>
+  <Grid item xs>
+      <Card raised >
+          <CardMedia component="img" sx={{ height: 200 }} image={dbImg}></CardMedia>
+          <Stack direction={"row"} spacing={1} p={2}>
+              {skillsData.databases.map((db) => (
+                  <Chip label={db.name} color="primary" size="small" />
+              ))}
+          </Stack>
+      </Card>
+  </Grid>
+  <Grid item xs>
+      <Card raised>
+          <CardMedia component="img" sx={{ height: 200 }} image={toolImg}></CardMedia>
+          <Stack direction={"row"} spacing={1} p={2}>
+              {skillsData.tools.map((tool) => (
+                  <Chip label={tool.name} color="primary" size="small" />
+              ))}
+          </Stack>
+      </Card>
+  </Grid>
+</Grid>
 }
 
 export default Skills;
