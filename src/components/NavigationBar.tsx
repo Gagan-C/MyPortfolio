782c9502206a147cf16b6a47b070f4f5b7c8f625
Gagan Chatu <67225429+Gagan-C@users.noreply.github.com>
2024-05-03 01:31:39 -0500

    PRWA-13 fixed nav bar responsive problem

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,4 +1,15 @@
+import { Menu, MenuItem } from "@mui/material";
+import { useState } from "react";
+
 function NavigationBar() {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <>
       <nav className="bg-white border-gray-200 dark:bg-gray-900">
@@ -17,6 +28,7 @@ function NavigationBar() {
             className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
             aria-controls="navbar-default"
             aria-expanded="false"
+            onClick={handleButtonClick}
           >
             <span className="sr-only">Open main menu</span>
             <svg
@@ -35,6 +47,20 @@ function NavigationBar() {
               />
             </svg>
           </button>
+          <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleMenuClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem onClick={handleMenuClose} href="#projects" component="a">Projects</MenuItem>
+        <MenuItem onClick={handleMenuClose} href="#skills" component="a">Skills</MenuItem>
+        <MenuItem onClick={handleMenuClose} href="#education" component="a">Education</MenuItem>
+        <MenuItem onClick={handleMenuClose} href="#contact" component="a">Contact</MenuItem>
+      </Menu>
           <div className="hidden w-full md:block md:w-auto" id="navbar-default">
             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
