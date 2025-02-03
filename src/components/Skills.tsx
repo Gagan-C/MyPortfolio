fd48d06aa374ceecd6b88f54720bd96cf61402c1
Gagan Chatu <67225429+Gagan-C@users.noreply.github.com>
2025-02-02 22:42:51 -0600

    added icons to skills
    used font awesome show icons in skills section

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,28 +1,29 @@
+
 import { Grid } from "@mui/material";
 import skillsData from "./skill-list.json";
 import proImg from "../assets/programming.jpeg";
 import dbImg from "../assets/db.jpeg";
 import toolImg from "../assets/tools.jpeg";
-import frameworkImg from "../assets/framework.jpeg"
+import frameworkImg from "../assets/framework.jpeg";
 import SkillCard from "./SkillCard.tsx";
 
-
-
 function Skills() {
-  return <Grid container spacing={4} p={2} rowSpacing={2} columnSpacing={2} data-testid="skills-component">
-  <Grid item xs={12} sm={6} md={4} >
-      <SkillCard image={proImg} list={skillsData.languages.map((language) => language.name)}/>
-  </Grid>
-  <Grid item xs={12} sm={6} md={4}>
-      <SkillCard image={dbImg} list={skillsData.databases.map((db) => db.name)}/>
-  </Grid>
-  <Grid item xs={12} sm={6} md={4}>
-      <SkillCard image={toolImg} list={skillsData.tools.map((tool) => tool.name)}/>
-  </Grid>
-  <Grid item xs={12} sm={6} md={4} >
-      <SkillCard image={frameworkImg} list={skillsData.frameworks.map((framework) => framework.name)}/>
-  </Grid>
-</Grid>
+  return (
+    <Grid container spacing={4} p={2} rowSpacing={2} columnSpacing={2} data-testid="skills-component">
+      <Grid item xs={12} sm={6} md={4}>
+        <SkillCard image={proImg} list={skillsData.languages} />
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+        <SkillCard image={dbImg} list={skillsData.databases} />
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+        <SkillCard image={toolImg} list={skillsData.tools} />
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+        <SkillCard image={frameworkImg} list={skillsData.frameworks} />
+      </Grid>
+    </Grid>
+  );
 }
 
 export default Skills;
