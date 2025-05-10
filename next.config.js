d0cef9fd3e6a7f8d2e8f076ae57c3d3e0d0a8662
Gagan Chatu <67225429+Gagan-C@users.noreply.github.com>
2025-05-10 17:05:15 -0500

    Refactor Dockerfile stages and update next.config.js for standalone output

--- next.config.js
+++ next.config.js
@@ -1,19 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-images: {
-    remotePatterns: [
-        {
-            protocol: 'https',
-            hostname: 'e21qb2sohxwlyxkx.public.blob.vercel-storage.com',
-            pathname: '/**',
-        },
-        {
-            protocol: 'https',
-            hostname: 'brm-workforce.oracle.com',
-            pathname: '/**'
-        }
-    ],
-}
+    output: "standalone",
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'e21qb2sohxwlyxkx.public.blob.vercel-storage.com',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'brm-workforce.oracle.com',
+                pathname: '/**'
+            }
+        ],
+    },
 };
 
 export default nextConfig;
